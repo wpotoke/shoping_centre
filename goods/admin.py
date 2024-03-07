dbdfcf7e14e20dabdcc50705bfe92013ed1a712b
moroz <moroz@example.com>
2024-03-07 04:13:45 +0300

    after adding fixtures for goods

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from goods.models import Categories, Products
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    
--- main/views.py
+++ main/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
+from goods.models import Categories
+
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context = {
         "title": "Home - Главная",
-        "content": "Магазин мебели HOME"
+        "content": "Магазин мебели HOME",
+        'categories': categories
         }
 
     return render(request, "main/index.html", context)
