34118f01c1975e8b24fe4f3d577b166cbfb6f4c6
moroz <moroz@example.com>
2024-03-16 21:55:18 +0300

    Teplates for users app added

--- app/urls.py
+++ app/urls.py
@@ -22,12 +22,13 @@ from app import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("main.urls", namespace='main')),
-    path('catalog/', include('goods.urls', namespace='catalog'))
+    path("", include("main.urls", namespace="main")),
+    path("catalog/", include("goods.urls", namespace="catalog")),
+    path("user/", include("users.urls", namespace="user")),
     ]
 
 if settings.DEBUG:
     urlpatterns += [
-    path("__debug__/", include("debug_toolbar.urls")),
+        path("__debug__/", include("debug_toolbar.urls")),
     ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
