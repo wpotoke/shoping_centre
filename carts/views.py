3a7d54dc6c2444bb7407d0250917af61131d43ad
moroz <moroz@example.com>
2024-04-11 17:06:26 +0300

    Cart remove controller added, commit before ajax -> (java-script)

--- carts/views.py
+++ carts/views.py
@@ -27,5 +27,8 @@ def cart_change(request, product_slug):
     ...
 
 
-def cart_remove(request, product_slug): 
-    ...
+def cart_remove(request, cart_id):
+
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete()
+    return redirect(request.META['HTTP_REFERER'])
